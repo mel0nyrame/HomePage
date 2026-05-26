d86aadb614128e3944f3cf8663578cbbea797db7
Mel0ny <2189829539@qq.com>
2026-05-26 08:43:31 +0800

    🐞 fix(common): 修复访问无权限接口时候的返回值

--- homepage-common/src/main/java/com/homepage/common/exception/GlobalExceptionHandler.java
+++ homepage-common/src/main/java/com/homepage/common/exception/GlobalExceptionHandler.java
@@ -6,6 +6,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.security.authorization.AuthorizationDeniedException;
 import org.springframework.validation.FieldError;
 import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.NoHandlerFoundException;
 
+import javax.naming.AuthenticationException;
 import java.util.stream.Collectors;
 
 /**
@@ -65,6 +67,12 @@ public class GlobalExceptionHandler {
     @ResponseStatus(HttpStatus.METHOD_NOT_ALLOWED)
     public Response<Void> handleHttpRequestMethodNotSupportedException(HttpRequestMethodNotSupportedException e) {
         return Response.fail(ResponseCode.METHOD_NOT_ALLOWED);
+    }
+
+    @ExceptionHandler(AuthorizationDeniedException.class)
+    @ResponseStatus(HttpStatus.UNAUTHORIZED)
+    public Response<Void> handleAuthorizationDenied(AuthorizationDeniedException e) {
+        return Response.fail(ResponseCode.PERMISSION_DENIED);
     }
 
 }
