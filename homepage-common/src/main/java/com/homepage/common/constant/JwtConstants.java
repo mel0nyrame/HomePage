a5c2363984fa9b89e4eb824ffc6f6cbfb7e29ccb
Mel0ny <2189829539@qq.com>
2026-05-29 13:28:15 +0800

    🐞 fix(yaml): 修复敏感信息

--- homepage-common/src/main/java/com/homepage/common/constant/JwtConstants.java
+++ homepage-common/src/main/java/com/homepage/common/constant/JwtConstants.java
@@ -1,23 +1,32 @@
 package com.homepage.common.constant;
 
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
 /**
  * @Author Mel0ny
  * @Package com.homepage.common.constant
  * @Date 5/23/26 00:59
  * @description: JWT常量类
  */
+@Component
 public class JwtConstants {
-
-    // 防止实例化
-    private JwtConstants() {}
-
-    /**
-     * JWT算法密钥
-     */
-    public static final String JWT_SECRET = "a3WkYFJ7yhppb56tWQjkR6ju8vmszmR3syvsKbUn43ceNjkXAV";
 
     /**
      * token过期时间 2天 * 24小时 * 60分钟 * 60 秒 * 1000毫秒
      */
-    public static final Long JWT_TOKEN_EXPIRATION_TIME = 2 * 24 * 60 * 60 * 1000L ;
+    public static final Long JWT_TOKEN_EXPIRATION_TIME = 2 * 24 * 60 * 60 * 1000L;
+    /**
+     * JWT算法密钥
+     */
+    public static String JWT_SECRET;
+
+    @Value("${jwt.secret}")
+    private String secret;
+
+    @PostConstruct
+    public void init() {
+        JWT_SECRET = secret;
+    }
 }
